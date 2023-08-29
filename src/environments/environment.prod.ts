0791a4c2b474dcf2a28b4e7263fbf8f5e35d6d9b
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-29 11:38:53 -0300

    Configuracion de build de produccion para que deje de generar tags de estilos inline que violan el csp establecido por helmet del lado servidor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000', //CHANGE for PRODUCTION BUILD
+  apiUrl: '', //Will default to the current url of the server that served the frontend
 };
