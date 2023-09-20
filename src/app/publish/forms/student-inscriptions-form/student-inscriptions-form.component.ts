54047211908549ee9050e971f8da5c59bed8787e
ThomasOjeda <thomasojeda05@gmail.com>
2023-09-20 13:12:45 -0300

    NOTswrQk Studentinscriptions con filtro de unidad y sexo

--- src/app/publish/forms/student-inscriptions-form/student-inscriptions-form.component.ts
+++ src/app/publish/forms/student-inscriptions-form/student-inscriptions-form.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TransformationForm } from '../transformation-form';
 import { FilesService } from 'src/app/files/services/files.service';
 import { FileType } from 'src/app/files/model/file-type';
+import { DataCategoriesService } from '../../services/data-categories.service';
 
 @Component({
   selector: 'app-student-inscriptions-form',
@@ -13,20 +14,41 @@ export class StudentInscriptionsFormComponent
   implements OnInit, TransformationForm
 {
   yearInputControl = new FormControl(null, [Validators.required]);
+  unitInputControl = new FormControl();
+  genderInputControl = new FormControl();
 
   @Input() transformationBody!: FormGroup;
 
   availableYears: number[] = [];
+  availableUnits: any[] = [];
+  availableGenders: any[] = [];
 
-  constructor(private filesServ: FilesService) {}
+  constructor(
+    private filesServ: FilesService,
+    private dataCatServ: DataCategoriesService
+  ) {}
 
   ngOnInit(): void {
     this.transformationBody.addControl('year', this.yearInputControl);
+    this.transformationBody.addControl('unit', this.unitInputControl);
+    this.transformationBody.addControl('sex', this.genderInputControl);
+
+    this.unitInputControl.setValue(undefined);
+    this.genderInputControl.setValue(undefined);
 
     this.filesServ
       .filesQuery(undefined, FileType.STUDENT_INSCRIPTIONS)
       .subscribe((data) => {
         this.availableYears = data.result.map((file) => file.year).sort();
       });
+
+    this.dataCatServ.getUnits().subscribe((data: any) => {
+      this.availableUnits = data.result;
+      this.availableUnits.unshift({ _id: undefined, label: 'Sin filtro' });
+    });
+    this.dataCatServ.getGenders().subscribe((data: any) => {
+      this.availableGenders = data.result;
+      this.availableGenders.unshift({ _id: undefined, label: 'Sin filtro' });
+    });
   }
 }
