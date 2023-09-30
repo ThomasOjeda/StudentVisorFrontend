03d8de982da0bbce05b653268780b49536f98577
ThomasOjeda <thomasojeda05@gmail.com>
2023-09-30 12:31:46 -0300

    changed environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
-export const environment = {
+/* export const environment = {
   production: true,
   apiUrl: '', //Will default to the current url of the server that served the frontend
+}; */
+export const environment = {
+  production: true,
+  apiUrl: 'http://206.189.202.126:5000', //Will default to the current url of the server that served the frontend
 };
