83dccd95ac4e2417ea0eed8d6f933f8a0346e593
ThomasOjeda <thomasojeda05@gmail.com>
2023-09-30 12:43:10 -0300

    cambios archivo environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
-/* export const environment = {
-  production: true,
-  apiUrl: '', //Will default to the current url of the server that served the frontend
-}; */
 export const environment = {
   production: true,
-  apiUrl: 'http://206.189.202.126:5000', //Will default to the current url of the server that served the frontend
+  apiUrl: '', //Will default to the current url of the server that served the frontend
 };
