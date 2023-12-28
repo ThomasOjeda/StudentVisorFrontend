ec0effde2f4ea3440c13bc1e8602aa9a51f41a27
ThomasOjeda <tojeda@alumnos.exa.unicen.edu.ar>
2023-12-28 16:10:23 -0300

    XYrQ76eQ formulario de transformacion movimientos becas

--- src/app/publish/forms/student-scholarship-movements-form/student-scholarship-movements-form.component.ts
+++ src/app/publish/forms/student-scholarship-movements-form/student-scholarship-movements-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TransformationForm } from '../transformation-form';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FilesService } from 'src/app/files/services/files.service';
+import { FileType } from 'src/app/files/model/file-type';
 
 @Component({
   selector: 'app-student-scholarship-movements-form',
@@ -8,10 +10,70 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./student-scholarship-movements-form.component.css'],
 })
 export class StudentScholarshipMovementsFormComponent
-  implements TransformationForm, OnInit
+  implements OnInit, TransformationForm
 {
-  constructor() {}
-  transformationBody!: FormGroup<any>;
+  yearAInputControl = new FormControl<number | null | undefined>(undefined, [
+    Validators.required,
+  ]);
+  yearBInputControl = new FormControl<number | null | undefined>(
+    { value: undefined, disabled: true },
+    [Validators.required]
+  );
+  scholarshipTypeInputControl = new FormControl<string | null | undefined>(
+    { value: undefined, disabled: false },
+    [Validators.required]
+  );
 
-  ngOnInit(): void {}
+  @Input() transformationBody!: FormGroup;
+
+  availableStartYears: number[] = [];
+  availableEndYears: number[] = [];
+
+  schTypes = [
+    { label: 'progresar', value: 'student-scholarships-progresar' },
+    { label: 'belgrano', value: 'student-scholarships-belgrano' },
+  ];
+
+  constructor(private filesServ: FilesService) {}
+
+  ngOnInit(): void {
+    this.yearAInputControl.valueChanges.subscribe((data) => {
+      this.yearAValueChanged(data);
+    });
+
+    this.yearBInputControl.valueChanges.subscribe((data) => {
+      this.yearBValueChanged(data);
+    });
+
+    this.transformationBody.addControl('yearA', this.yearAInputControl);
+    this.transformationBody.addControl('yearB', this.yearBInputControl);
+    this.transformationBody.addControl(
+      'schType',
+      this.scholarshipTypeInputControl
+    );
+
+    this.filesServ
+      .filesQuery(undefined, FileType.STUDENT_INSCRIPTIONS)
+      .subscribe((data) => {
+        this.availableStartYears = data.result.map((file) => file.year).sort();
+      });
+  }
+
+  yearAValueChanged(yearA: number | null | undefined) {
+    this.reconfigureYearBInput(yearA);
+  }
+
+  yearBValueChanged($event: number | null | undefined) {}
+
+  reconfigureYearBInput(yearA: number | null | undefined) {
+    this.yearBInputControl.setValue(undefined);
+    this.yearBInputControl.disable({ emitEvent: false });
+    if (yearA) {
+      this.availableEndYears = this.availableStartYears.slice(
+        this.availableStartYears.lastIndexOf(yearA) + 1
+      );
+
+      this.yearBInputControl.enable({ emitEvent: false });
+    }
+  }
 }
