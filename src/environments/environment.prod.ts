2341792124be5135c21717b4b8ed27f2f2ccdd13
ThomasOjeda <tojeda@alumnos.exa.unicen.edu.ar>
2024-03-19 21:14:01 -0300

    reconfigured environment api paths

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://206.189.202.126:5000', //Will default to the current url of the server that served the frontend
+  apiUrl: '', //Will default to the current url of the server that served the frontend
 };
